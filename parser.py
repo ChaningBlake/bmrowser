1b3cdc83603d17767c64a28fb727bec6130f5ad3
Blake Mullinax <blakemullinax@charter.net>
2020-05-15 21:34:16 -0500

    added node class

--- parser.py
+++ parser.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+class Node(self):
+    def __init__(self):
+        self.data = None
+        self.left = None
+        self.right = None
+
 def parse(html_string):
 
     return
